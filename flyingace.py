2643837e31f8d99727ca336825d04f9ea63fff1c
jedwilshire <84249299+jedwilshire@users.noreply.github.com>
2021-09-05 14:35:59 -0500

    basic starter program outline

---
+++ flyingace.py
@@ -0,0 +1,67 @@
+import pygame as pg
+import os
+class Color:
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    LIGHT_BLUE = (135,206,250)
+
+class Format:
+    WIDTH = 640
+    HEIGHT = 480
+    FPS = 30
+    game_folder = os.path.dirname(__file__)
+    img_folder = os.path.join(game_folder, 'images')
+    sounds_folder = os.path.join(game_folder, 'sounds')
+# define colors
+
+
+class Application:
+    def __init__(self):
+        self.running = True
+        self.clock = pg.time.Clock()
+        self.screen = pg.display.set_mode((Format.WIDTH, Format.HEIGHT))
+        self.sprites = pg.sprite.Group()
+        pg.display.set_caption('Flying Ace!')
+        
+        
+    def gameloop(self):
+        while self.running:
+            self.clock.tick(Format.FPS)
+            # Process input (events)
+            for event in pg.event.get():
+                # check for closing window
+                if event.type == pg.QUIT:
+                    self.running = False
+                elif event.type == pg.KEYDOWN:
+                    self.handle_keydown(event.key)
+                elif event.type == pg.KEYUP:
+                    self.handle_keyup(event.key)
+            # Update
+            self.sprites.update()
+            # Draw / render
+            self.screen.fill(Color.LIGHT_BLUE)
+            self.sprites.draw(self.screen)
+            # *after* drawing everything, flip the display
+            pg.display.flip()
+    
+    def handle_keydown(self, key):
+        if key == pg.K_w:
+            print('w down')
+    
+    def handle_keyup(self, key):
+        if key == pg.K_w:
+            print('w up')
+   
+    
+def main():
+    pg.init()
+    app = Application()
+    app.gameloop()
+    pg.quit()
+    
+
+if __name__ == '__main__':
+    main()
